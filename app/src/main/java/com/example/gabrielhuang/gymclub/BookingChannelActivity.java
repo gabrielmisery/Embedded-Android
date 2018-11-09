ec8978044a65855edffc09d30d9ab3fcca469613
gabrielmisery <gabriel.misery@gmail.com>
2018-11-09 14:03:56 +0800

    booking class 界面初步完成

---
+++ app/src/main/java/com/example/gabrielhuang/gymclub/BookingChannelActivity.java
@@ -0,0 +1,13 @@
+package com.example.gabrielhuang.gymclub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BookingChannelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_booking_channel);
+    }
+}
--- app/src/main/java/com/example/gabrielhuang/gymclub/ChannelFragment.java
+++ app/src/main/java/com/example/gabrielhuang/gymclub/ChannelFragment.java
@@ -1,17 +1,24 @@
 package com.example.gabrielhuang.gymclub;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ChannelFragment extends Fragment {
+    LinearLayout pushUps;
 
 
     public ChannelFragment() {
@@ -23,7 +30,23 @@ public class ChannelFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_channel, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        pushUps = getActivity().findViewById(R.id.pushUps);
+
+        pushUps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), BookingChannelActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
